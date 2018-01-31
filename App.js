a28bcd09fd6d5c5ed54526003abd0653f9b0325f
jmoliugp <juan@xmartlabs.com>
2018-01-31 17:40:18 -0300

    added a header bar and a section list with styles

--- App.js
+++ App.js
@@ -1,37 +1,26 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry, SectionList, StyleSheet, Text, View } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-export default class App extends Component<{}> {
+export default class SectionListBasics extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <View style={styles.headerBar}>
+          <View style={styles.headerBarTextContainer}>
+            <Text style={styles.headerBarText}>
+              TodoApp
+            </Text>
+          </View>
+        </View>
+        <SectionList
+          sections={[
+            {title: 'Pending', data: ['Cofee', 'Fruit']},
+            {title: 'Done', data: ['Monitors', 'Notebooks', 'PCs']},
+          ]}
+          renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
+          renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+          keyExtractor={(item, index) => index}
+        />
       </View>
     );
   }
@@ -40,18 +29,50 @@ export default class App extends Component<{}> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 22,
+  },
+  headerBar: {
+    flex: 0,
+    flexBasis: 50,
+    height: 50,
+    backgroundColor: 'steelblue',
+  },
+  headerBarTextContainer: {
+    flex: 85,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    color: 'white',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  headerBarText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontFamily: 'verdana',
+    fontSize: 30,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  headerBarButtonContainer: {
+    flex: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: 'white',
   },
-});
+  headerBarButton: {
+    color: 'white',
+  },
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(247,247,247,1.0)',
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+})
+
+// skip this line if using Create React Native App
+AppRegistry.registerComponent('AwesomeProject', () => SectionListBasics);
